2ab3ec531debd269cdc0b74dd8ba94e9060db4f8
krischal111 <67739761+krischal111@users.noreply.github.com>
2023-12-12 08:42:17 +0545

    I commit.

---
+++ 1464-maximum-prod.py
@@ -0,0 +1,17 @@
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         a, b = sorted(nums)[-2:]
+#         print(a,b)
+#         return (a-1)*(b-1)
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        a, b = nums[:2]
+        a, b = (a,b) if a <= b else (b, a)
+        for num in nums[2:]:
+            if num >= b:
+                a = b
+                b = num
+            elif num >= a:
+                a = num
+
+        return (a-1)*(b-1)
