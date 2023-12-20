f8fbc119f33025f35a2f1c271fc2e34c67aa9523
krischal111 <67739761+krischal111@users.noreply.github.com>
2023-12-20 17:23:53 +0545

    Today's solution

---
+++ 2706-2-chocolate.py
@@ -0,0 +1,19 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        price_lowest, price_lower = sorted(prices[:2])
+
+        # we need the two smallest prices
+        for new_price in prices[2:]:
+            if new_price < price_lower:
+                if new_price < price_lowest:
+                    price_lower = price_lowest
+                    price_lowest = new_price
+                else:
+                    price_lower = new_price
+        sum_cost = price_lowest + price_lower
+        if sum_cost > money:
+            return money
+        else:
+            return money - sum_cost
+
+        
